c14d8d8ad95cfc7a24d0596e815323e4c4a3cc67
Miguel Karlo Sese <sese.miguel9@gmail.com>
2022-10-23 12:31:18 +0800

    Initial version

---
+++ first-react-pratice/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <ul>
+        <li>First bullet</li>
+        <li>Second bullet</li>
+    </ul>
+    ,
+    document.getElementById("root")
+);
